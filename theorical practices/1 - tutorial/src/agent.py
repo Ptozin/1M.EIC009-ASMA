3802a19fb32c34c82575017ac380032740219d64
DiogoSilva11 <up202004288@up.pt>
2024-03-04 16:52:34 +0000

    agent communication

--- theorical practices/1 - tutorial/src/agent.py
+++ theorical practices/1 - tutorial/src/agent.py
@@ -1,15 +1,63 @@
 import spade
+from spade.agent import Agent
+from spade.behaviour import OneShotBehaviour
+from spade.message import Message
+from spade.template import Template
 from create_user import ProsodyClient
 
-class DummyAgent(spade.agent.Agent):
+class SenderAgent(Agent):
+    class InformBehav(OneShotBehaviour):
+        async def run(self):
+            print("InformBehav running")
+            msg = Message(to="user_1@localhost")     # Instantiate the message
+            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+            msg.body = "Hello World"                    # Set the message content
+
+            await self.send(msg)
+            print("Message sent!")
+
+            # stop agent from behaviour
+            await self.agent.stop()
+
     async def setup(self):
-        print("Hello World! I'm agent {}".format(str(self.jid)))
+        print("SenderAgent started")
+        b = self.InformBehav()
+        self.add_behaviour(b)
+
+class ReceiverAgent(Agent):
+    class RecvBehav(OneShotBehaviour):
+        async def run(self):
+            print("RecvBehav running")
+
+            msg = await self.receive(timeout=10) # wait for a message for 10 seconds
+            if msg:
+                print("Message received with content: {}".format(msg.body))
+            else:
+                print("Did not received any message after 10 seconds")
+
+            # stop agent from behaviour
+            await self.agent.stop()
+
+    async def setup(self):
+        print("ReceiverAgent started")
+        b = self.RecvBehav()
+        template = Template()
+        template.set_metadata("performative", "inform")
+        self.add_behaviour(b, template)
 
 async def main():
-    dummy = DummyAgent("admin@localhost", "admin")
-    await dummy.start()
+    receiveragent = ReceiverAgent("user_1@localhost", "admin")
+    await receiveragent.start(auto_register=True)
+    print("Receiver started")
+
+    senderagent = SenderAgent("admin@localhost", "admin")
+    await senderagent.start(auto_register=True)
+    print("Sender started")
+
+    await spade.wait_until_finished(receiveragent)
+    print("Agents finished")
 
 if __name__ == "__main__":
-    spade.run(main())
-    client = ProsodyClient(container_id="123456789abcdef")
+    client = ProsodyClient(container_id="cac0644e613baaabb491dc8574f63db61e94a3e03e02d94b365b2d93430bcab1")
     client.create_user("user_1")
+    spade.run(main())
